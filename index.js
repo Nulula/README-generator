cbf03ad4bd7d8577545f521017083a6b3fa62ffb
Nulula <nulula@gmail.com>
2023-02-18 14:36:26 +0000

    questions in progress

--- index.js
+++ index.js
@@ -5,6 +5,36 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+    {
+    type: 'input',
+    message: 'What is the title of your project?',
+    name: 'title'
+    },
+    {
+    type: 'input',
+    message: 'What is your GitHub username?',
+    name: 'username'
+    },
+    {
+    type: 'input',
+    message: 'Can you provide a short description of your project?',
+    name: 'description'
+    },
+    {
+    type: 'input',
+    message: 'Can you provide the installation instructions?',
+    name: 'installation'
+    },
+    {
+    type: 'input',
+    message: 'Can you provide the usage information?',
+    name: 'usage'
+    },
+    {
+    type: 'input',
+    message: 'Can you provide the usage information?',
+    name: 'usage'
+    },
 
 ];
 
