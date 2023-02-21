50a68e0b48022273425b06330745e9e1e930cb4f
Nulula <nulula@gmail.com>
2023-02-21 22:55:45 +0000

    entire app functions correctly

---
+++ README-example.md
@@ -0,0 +1,34 @@
+
+  # dfsd
+
+  ![](https://img.shields.io/badge/license-cc0%201.0-green)
+  
+  ## Description
+  sdfsdf
+  ## Table of contents
+  [Installation](#installation)
+  
+  [Usage](#usage)
+  
+  [License](#license)
+  
+  [Contribution](#contribution)
+  
+  [Tests](#tests)
+  
+  [Questions](#questions)
+  
+  ## Installation
+  sdfs
+  ## Usage
+  sdfsdf
+  ## License
+  cc0 1.0
+  ## Contribution
+  dfs
+  ## Tests
+  sdfsfd
+  ## Questions
+  Repository adress: https://github.com/sdfs
+  
+  Please direct questions to sdfsdf
--- index.js
+++ index.js
@@ -8,90 +8,85 @@ const questions = [
     {
     type: 'input',
     message: 'What is the title of your project?',
-    name: 'title'
+    name: 'title',
+    validate: function(input) {
+        return input !== '';
+      }
     },
     {
     type: 'input',
     message: 'What is your GitHub username?',
-    name: 'username'
+    name: 'username',
+    validate: function(input) {
+        return input !== '';
+      }
     },
     {
     type: 'input',
     message: 'What is your email address?',
     name: 'email',
+    validate: function(input) {
+        return input !== '';
+      }
     },
     {
     type: 'input',
     message: 'Can you provide a short description of your project?',
-    name: 'description'
+    name: 'description',
+    validate: function(input) {
+        return input !== '';
+      }
     },
     {
     type: 'input',
     message: 'Can you provide the installation instructions?',
-    name: 'installation'
+    name: 'installation',
+    validate: function(input) {
+        return input !== '';
+      }
     },
     {
     type: 'input',
     message: 'Can you provide the usage information?',
-    name: 'usage'
+    name: 'usage',
+    validate: function(input) {
+        return input !== '';
+      }
     },
     {
     type: 'list',
     message: 'Please choose the license type',
     name: 'license',
-    choices: ['afl-.0','apache-2.0','artistic-2.0','bsl-1.0','bsd-2-clause','bsd-3-clause','bsd-3-clause-clear','cc','cc0-1.0','cc-by-4.0','cc-by-sa-4.0','wtfpl','ecl-2.0','epl-1.0','epl-2.0','eupl-1.1','agpl-3.0','gpl','gpl-2.0','gpl-3.0','lgpl','lgpl-2.1','lgpl-3.0','isc','lppl-1.3c','ms-pl','mit','mpl-2.0','osl-3.0','postgresql','ofl-1.1','ncsa','unlicense','zlib']
+    choices: ['afl 1.0','apache 2.0','artistic 2.0','bsl 1.0','bsd 2 clause','bsd 3 clause','bsd 3 clause clear','cc','cc 1.0','cc by 4.0','cc by sa 4.0','wtfpl','ecl 2.0','epl 1.0','epl 2.0','eupl 1.1','agpl 3.0','gpl','gpl 2.0','gpl 3.0','lgpl','lgpl 2.1','lgpl 3.0','isc','lppl 1.3c','ms pl','mit','mpl 2.0','osl 3.0','postgresql','ofl 1.1','ncsa','unlicense','zlib']
     },
     {
     type: 'input',
     message: 'How can people contribute to your project?',
-    name: 'contributing'
+    name: 'contributing',
+    validate: function(input) {
+        return input !== '';
+      }
     },
     {
     type: 'input',
     message: 'How did you test your application?',
-    name: 'tests'
+    name: 'tests',
+    validate: function(input) {
+        return input !== '';
+      }
     },
 
 ];
 
 // // function to write README file
 function writeToFile(fileName, data) {
-    fs.appendFile(fileName,`
-    # ${data.title}
-
-    ![](https://img.shields.io/badge/license-${data.license}-green)
-    
-    ## Description
-    ${data.description}
-    ## Table of contents
-    [Installation](#installation)
-    
-    [Usage](#usage)
-    
-    [License](#license)
-    
-    [Contribution](#contribution)
-    
-    [Tests](#tests)
-    
-    [Questions](#questions)
-    
-    ## Installation
-    ${data.installation}
-    ## Usage
-    ${data.usage}
-    ## License
-    ${data.license}
-    ## Contribution
-    ${data.contribution}
-    ## Tests
-    ${data.tests}
-    ## Questions
-    Repository adress: https://github.com/${data.username}
-    Please direct questions to ${data.email}
-    `,(err) =>
-    err ? console.error(err) : console.log('Something went wrong, please try again')
-  );
+    fs.writeFile(fileName,generateMarkdown(data),(err) =>{
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(`File '${fileName}' has been created.`);
+        }
+    });
 };
 
 // // function to initialize program
@@ -102,18 +97,16 @@ function init() {
     Welcome to README file generator. Please answer a couple of questions,
     and your README file will be automatically generated. Let's go!
     `);
-};
+    inquirer.prompt(
+        questions
+        ).then((responses) => {
+        writeToFile('README-example.md',responses)
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+    };
 
 
 // // function call to initialize program
 init();
-inquirer.prompt(
-    questions
-    ).then((responses) => {
-    if (questions.validate === false) {
-        console.log("This is no a valid email address, please start again");
-        init()
-    } else {
-        writeToFile('README-example.md',responses)
-    }
-})
